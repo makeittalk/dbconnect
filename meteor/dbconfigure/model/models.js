dbca2f3c81667cb55ad3d7e79ff233d9d80f80ad
Make it talk <make.db.talk@gmail.com>
2014-09-12 11:53:47 -0400

    Add dynamic SimpleSchema builder.  Changes to be committed: 	modified:   model/models.js

--- meteor/dbconfigure/model/models.js
+++ meteor/dbconfigure/model/models.js
@@ -9,3 +9,16 @@ Fields.attachSchema(Schema.Fields);
 
 SchemaDefinition = new Mongo.Collection("schemas");
 SchemaDefinition.attachSchema(Schema.SchemaDefinition);
+
+var f = Fields.find();
+
+var oneField={};
+f.forEach(function(field){
+  oneField[field.name]={};
+  oneField[field.name]["type"]=field.type;
+  oneField[field.name]["label"]=field.label;
+});
+
+Tasks.attachSchema(oneField);
+Servers.attachSchema(oneField);
+
